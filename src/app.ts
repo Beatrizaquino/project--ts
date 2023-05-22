0219a26fbfd0700322fa7feb15c6bdd00e6c3536
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-05-22 17:11:52 -0400

    autobind

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 
+//autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 
+    const originalMathod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMathod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+
+//project input class
 class ProjectInput  {
 
     templateElement: HTMLTemplateElement ;
     hostElement: HTMLDivElement ;
     element: HTMLFormElement ;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputeElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -12,11 +30,29 @@ class ProjectInput  {
 
         const importNode = document.importNode(this.templateElement.content, true);
         this.element = importNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputeElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
+    @autobind
+    private submitHanlder(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHanlder.bind(this));
+    }
+
     private attach() {
-        this.hostElement?.insertAdjacentElement(`afterbegin`, this.element );
+        this.hostElement?.insertAdjacentElement('afterbegin', this.element );
     }
 }
 
