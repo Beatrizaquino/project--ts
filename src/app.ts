9c689066ad5c1956469ba21b1ba9eceb3dd51668
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-05-23 14:22:18 -0400

    validation function

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,35 @@
 
+//validation
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable){
+    let isvalid = true;
+    if ( validatableInput.required) {
+        isvalid = isvalid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isvalid = isvalid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isvalid = isvalid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isvalid = isvalid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isvalid = isvalid && validatableInput.value <= validatableInput.max;
+    }
+    return isvalid;
+}
+
+
 //autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 
@@ -40,13 +71,32 @@ class ProjectInput  {
         this.attach();
     }
 
-    private gatheruserInput(): [string, string, number] | undefined{
+    private gatherUserInput(): [string, string, number] | undefined{
         const enteredTitle = this.titleInputElement.value;
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputeElement.value;
 
-        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0){
-            alert('Invalid input. please try again');
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1
+        };
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable) 
+        ){
+            alert('Invalid input, please try again');
             return;
         } else {
             return [enteredTitle,enteredDescription, +enteredPeople];
@@ -62,7 +112,7 @@ class ProjectInput  {
     @autobind
     private submitHanlder(event: Event) {
         event.preventDefault();
-        const userInput = this.gatheruserInput();
+        const userInput = this.gatherUserInput();
 
         if(Array.isArray(userInput)){
             const [ title, desc, people] = userInput;
